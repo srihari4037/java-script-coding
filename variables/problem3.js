e18256f2efc96207feadfb242a719e8648583081
srihari4037 <>
2022-10-14 17:55:31 +0530

    folder1

---
+++ variables/problem3.js
@@ -0,0 +1,9 @@
+//Create a variable name to store your name
+//Create a variable age to store your age
+//Print the values stored in the variable on one line followed by the type of the variable in the next line
+
+var name="srihari";
+var age=21;
+
+console.log("name:",name);
+console.log("age:",age);
---
+++ variables/problem4.js
@@ -0,0 +1,21 @@
+//Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+//Print the report card of the student (You can make it look nice by using some keyboard symbols )
+//Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+var name="srihari";
+var school="MJP,BONAKAL";
+var grade="A";
+var section="B";
+var rollno=26;
+
+console.log("**REPORT**");
+console.log("SCHOOL=",school);
+console.log("***~~~***");
+console.log("NAME=",name);
+console.log("***~~~***");
+console.log("SEC=",section)
+console.log("***~~~***");
+console.log("ROLLNO=",rollno);
+console.log("***~~~***");
+console.log("GRADE=",grade);
+console.log("***~~~***");
